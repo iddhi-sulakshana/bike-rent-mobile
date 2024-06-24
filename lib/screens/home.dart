1901b1e56678cbe290c0bb52f2524d4e3dc25bab
Iddhi Dasanayaka <iddhidasanayaka@gmail.com>
2024-06-24 11:31:23 +0530

    added bottom nav bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,7 @@
+import 'package:bike_rent_mobile/components/custom_botton_navbar.dart';
 import 'package:bike_rent_mobile/features/user_auth/firebase_auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,6 +11,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+  }
+
   int _counter = 0;
 
   void _incrementCounter() {
@@ -20,35 +35,35 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            // space
-            const SizedBox(height: 20),
-            // signout button
-            ElevatedButton(
-              onPressed: () {
-                // signout user
-                FirebaseAuthService().signOut(context);
-              },
-              child: const Text('Sign Out'),
-            ),
-          ],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              // space
+              const SizedBox(height: 20),
+              // signout button
+              ElevatedButton(
+                onPressed: () {
+                  // signout user
+                  FirebaseAuthService().signOut(context);
+                },
+                child: const Text('Sign Out'),
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ),
+        bottomNavigationBar: const CustomBottomNavBar(selectedIndex: 0));
   }
 }
