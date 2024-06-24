063d59b599894e75dbb6129e2686cc95a90fa340
Iddhi Dasanayaka <iddhidasanayaka@gmail.com>
2024-06-24 10:11:52 +0530

    Added No appbar for application

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:bike_rent_mobile/firebase_options.dart';
 import 'package:bike_rent_mobile/my_app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -3,11 +3,39 @@ import 'package:bike_rent_mobile/screens/login.dart';
 import 'package:bike_rent_mobile/screens/signup.dart';
 import 'package:bike_rent_mobile/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    // Add an observer to listen for app lifecycle changes
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    if (state == AppLifecycleState.resumed) {
+      // Ensure status bar remains hidden when app resumes
+      SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+    }
+  }
+
+  @override
+  void dispose() {
+    // Remove the observer when the widget is disposed
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
